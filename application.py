7bbf33b600799512220a35f582de3545c881c525
Rahul Jaisimha <rahul@jaisimha.com>
2020-01-05 16:37:40 +0530

    Use base36 for id

--- application.py
+++ application.py
@@ -75,7 +75,24 @@ def encodeColorGrid(color_grid):
     for row in color_grid:
         for color in row:
             code += color_codes[color]
-    return int(code, 4)
+    return base36encode(int(code, 4))
+
+def base36encode(number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+    base36 = ''
+    sign = ''
+    if number < 0:
+        sign = '-'
+        number = -number
+    if 0 <= number < len(alphabet):
+        return sign + alphabet[number]
+    while number != 0:
+        number, i = divmod(number, len(alphabet))
+        base36 = alphabet[i] + base36
+    return sign + base36
+
+
+def base36decode(number):
+    return int(number, 36)
     
 
 if(__name__ == "__main__"):
